465468713ca9426205864d54fbd99f6debc7f893
kimkr <dev.kimkr@gmail.com>
2018-05-18 17:10:29 +0900

    Connect starting and stoping GCE instances to REST API

--- server/compute/index.js
+++ server/compute/index.js
@@ -1,7 +1,7 @@
 const Compute = require('@google-cloud/compute');
 const compute = new Compute();
 
-exports.listVMs = function (options, callback) {
+exports.listVMs = (options, callback) => {
   compute.getVMs(options, (err, vms) => {
     console.log(JSON.stringify(options), JSON.stringify(err), JSON.stringify(vms));
     if (err) {
@@ -10,4 +10,27 @@ exports.listVMs = function (options, callback) {
     console.log(vms);
     callback(vms);
   });
+}
+
+exports.createVM = (name, zone, options) => {
+  var zone = compute.zone(zone || 'us-central1-a');
+  var name = name || 'gce-vm';
+  var { os } = options;
+  return zone.createVM(name, { os: os || 'ubuntu' })
+    .then(data => {
+      const vm = data[0];
+      const operation = data[1];
+      return operation.promise();
+    });
+}
+
+exports.deleteVM = (name, zone) => {
+  var zone = compute.zone(zone);
+  var vm = zone.vm(name);
+  return vm.delete()
+    .then(data => {
+      console.log('Deleting ' + name + '...');
+      const operation = data[0];
+      return operation.promise();
+    });
 }
--- server/index.js
+++ server/index.js
@@ -5,14 +5,36 @@ const compute = require('./compute');
 const app = express();
 const PORT = 8080;
 
-var simpleGetResHandler = function (path, callback) {
-  app.get(path, callback)
+var simpleGetResHandler = (path, message) => {
+  app.get(path, (req, res) => res.send(message))
 };
 
-simpleGetResHandler('/', (req, res) => res.send('gcp-cleaner is running...'));
-simpleGetResHandler('/start', (req, res) => res.send('start'));
-simpleGetResHandler('/stop', (req, res) => res.send('stop'));
-simpleGetResHandler('/status', (req, res) => compute.listVMs({}, list => res.send(JSON.stringify(list))));
+simpleGetResHandler('/', 'gcp-cleaner is running...');
+
+app.get('/stop/:name', (req, res) => {
+  compute.deleteVM(req.params.name, 'us-central1-c')
+    .then(() => res.send('VM is successfully deleted.'))
+    .catch(err => {
+      console.error(err)
+      res.code(400).send(JSON.stringify(err))
+    })
+});
+
+app.get('/start', (req, res) => {
+  var timestamp = Math.floor(Date.now() / 1000);
+  compute.createVM('ein-gcp-cleaner' + timestamp, 'us-central1-c', { os: 'ubuntu' })
+    .then(() => res.send('VM is successfully created.'))
+    .catch(err => {
+      console.error(err)
+      res.code(400).send(JSON.stringify(err))
+    })
+});
+
+app.get('/status', (req, res) => {
+  compute.listVMs({}, list => {
+    res.send(JSON.stringify(list))
+  })
+});
 
 app.listen(PORT, function () {
   console.log('gcp-cleaner is listening on port ' + PORT);
